8a96d81d2b2751a4e93541ebabe10395657a3c5f
adamlewandowski2000-wq <adam.lewandowski2000@wp.pl>
2026-02-02 18:57:40 +0100

    Update MiniSklepLiquidow.js

--- src/MiniSklepLiquidow.js
+++ src/MiniSklepLiquidow.js
@@ -2,7 +2,7 @@
 import { useState, useEffect } from "react";
 import bg from "./assets/bg-liquid.png";
 
-const SHEET_API = "https://script.google.com/macros/s/AKfycbwg66XvHMHS6TkIYdpqAPw0pUOgVIZWaf9HLplj_HkapRJQ7Fy-tC6--VCfGNWpgOv1/exec";
+const SHEET_API = "https://script.google.com/macros/s/AKfycbzuSqWcxanyQO-BEEDkCrYqijXWUgjZFXNVAes6cUr_4jJqzypM4xgzvKPmt5zp5tNW/exec";
 
 export default function MiniSklepLiquidow() {
   const [inventory, setInventory] = useState({});
